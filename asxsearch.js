da4e49d82a40fd01ee3e31fd926315461313a0f2
Mike Cunneen <mike@cunneen.biz>
2014-10-17 00:05:25 +0800

    Add yahoo finance integration.

--- asxsearch.js
+++ asxsearch.js
@@ -1,14 +1,18 @@
 
 if (Meteor.isClient) {
+  var progressNumTasks = 0, progressTasksCompleted = 0;
+
   Meteor.startup(function () {
     $("#excel").addClass("hidden");
   });
 
   var spinner = new Spinner();
+
   // counter starts at 0
   Session.setDefault("counter", 0);
   Session.setDefault("ASXResults", []);
 
+  console.log(Template.hello);
 
   Template.hello.helpers({
     counter: function () {
@@ -16,12 +20,41 @@ if (Meteor.isClient) {
     },
     company: function () {
       return Session.get("ASXResults");
+    },
+    companyName: function () {
+      return this["ticker"];
+    },
+    companyTicker: function () {
+      return this["ticker"];
+    },
+    companyProp: function () {
+
+      var props = _.map(this,
+              function (val, key) {
+                var obj = {};
+                obj["key"] = key;
+                obj["value"] = val;
+
+                return obj;
+              });
+
+      return props;
     }
   });
 
+  function updateProgress() {
+    var progressBar = $("#progressBar")[0];        // increment the counter when button is clicked
+    var percentage = ((progressTasksCompleted / progressNumTasks) * 100).toFixed(0);
+    progressBar.style.width = "" + percentage + "%";
+    progressBar.innerHTML = percentage + "% Complete.";
+    if (progressTasksCompleted === progressNumTasks) {
+      spinner.stop();
+      $("#excel").removeClass("hidden");
+    }
+  }
+
   Template.hello.events({
     'click button': function () {
-      // increment the counter when button is clicked
       Session.set("ASXResults", []);
 
       var tickerCodes = $('#tickercodes').val();
@@ -32,24 +65,29 @@ if (Meteor.isClient) {
       } else {
         toastr["error"]("Enter a valid stock ticker");
       }
-      var url;
-      for (var i = 0; i < tickersArray.length; i++) {
+      var url, i;
+      progressNumTasks = tickersArray.length * 2;
+      progressTasksCompleted = 0;
+      $("#excel").addClass("hidden");
+      for (i = 0; i < tickersArray.length; i++) {
         spinner.spin(document.body);
         Meteor.call("getASXData",
                 tickersArray[i],
                 function (error, result) {
-                  spinner.stop();
+                  progressTasksCompleted++;
+                  updateProgress();
                   if (error || !(!!result)) {
                     console.log(error);
                     toastr["error"]("There was an issue.");
                   } else {
-                    var newObj = {};
+                    var ticker = result[1];
+                    result = result[0];
+                    var newObj = {"ticker": ticker};
                     var html = $.parseHTML(result.content);
                     var responseJQuery = $(html);
                     var companyNameElem = responseJQuery.find("#company-information  h1")[0];
                     if (!!companyNameElem) {
                       toastr["success"]("Great success!");
-                      $("#excel").removeClass("hidden");
                     } else {
                       toastr["warning"]("Strange result. Please check your ticker.");
                       return;
@@ -62,11 +100,14 @@ if (Meteor.isClient) {
                     for (var i = 0; i < companyDetailsHeadings.length; i++) {
                       newObj[companyDetailsHeadings[i].innerText] = companyDetailsValues[i].innerText;
                     }
-                    // Hopefully by now we have the issuer code
-                    var ticker = newObj["Issuer Code"];
-                    // index it in our session by ticker.
-                    var ASXResults = Session.get("ASXResults");
-                    ASXResults.push(newObj);
+                    // find our ticker's object in the session if it's there.
+                    var ASXResults = Session.get("ASXResults") || [];
+                    var sessionObj = _.findWhere(ASXResults, {"ticker": ticker}) || {};
+                    ASXResults = _.without(ASXResults, sessionObj);
+
+                    sessionObj = _.extend(sessionObj, newObj);
+                    ASXResults.push(sessionObj);
+
                     Session.set("ASXResults", ASXResults);
 
 
@@ -74,8 +115,55 @@ if (Meteor.isClient) {
                   }
                 }
         );
+        Meteor.call("getYahooFinanceData",
+                tickersArray[i],
+                function (error, result) {
+                  progressTasksCompleted++;
+                  updateProgress();
+                  if (error || !(!!result)) {
+                    console.log(error);
+                    toastr["error"]("There was an issue.");
+                  } else {
+                    var ticker = result[1];
+                    result = result[0];
+                    var newObj = {"ticker": ticker};
+                    var responseJSON = JSON.parse(result.content);
+                    if (!!responseJSON) {
+                      toastr["success"]("Yahoo!");
+                      var value;
+                      for (var key in responseJSON.query.results.stats) {
+
+                        var tempval = responseJSON.query.results.stats[key];
+                        if (typeof tempval === "object") {
+                          if (tempval.content) {
+                            value = tempval.content;
+                          } else {
+                            value = JSON.stringify(tempval);
+                          }
+                        } else {
+                          value = tempval;
+                        }
+                        newObj[key] = value;
+                      }
+                      var ASXResults = Session.get("ASXResults") || [];
+                      // find our ticker's object in the session if it's there.
+                      var sessionObj = _.findWhere(ASXResults, {"ticker": ticker}) || {};
+                      ASXResults = _.without(ASXResults, sessionObj);
+                      sessionObj = _.extend(sessionObj, newObj);
+                      ASXResults.push(sessionObj);
+
+                      Session.set("ASXResults", ASXResults);
+                    } else {
+                      toastr["warning"]("Strange resultfrom yahoo. Please check your ticker.");
+                      return;
+                    }
+
+                  }
+                }
+        );
       }
     },
+    // ========= build a spreadsheet for download
     'click #excel': function () {
 
       var ASXResults = Session.get("ASXResults");
@@ -95,7 +183,7 @@ if (Meteor.isClient) {
       var d = new Date();
       var t = d.toTimeString().substring(0, 8).replace(/:/g, "-"); // time component.
       var currentTimeString = "" + d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate() + "_" + t;
-
+      // ==== create a link to download the spreadsheet
       var a = document.createElement("A");
       a.setAttribute("id", "downloadLink");
 
@@ -127,6 +215,7 @@ if (Meteor.isClient) {
   });
 }
 
+
 if (Meteor.isServer) {
   Meteor.startup(function () {
 
@@ -134,7 +223,16 @@ if (Meteor.isServer) {
   Meteor.methods({getASXData: function (ticker) {
       try {
         var result = HTTP.get("http://www.asx.com.au/asx/research/companyInfo.do?by=asxCode&asxCode=" + ticker, null);
-        return result;
+        return [result, ticker];
+      } catch (e) {
+        // Got a network error, time-out or HTTP error in the 400 or 500 range.
+        return false;
+      }
+    }});
+  Meteor.methods({getYahooFinanceData: function (ticker) {
+      try {
+        var result = HTTP.get("https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20yahoo.finance.keystats%20WHERE%20symbol%3D'" + ticker + ".AX'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=", null);
+        return [result, ticker];
       } catch (e) {
         // Got a network error, time-out or HTTP error in the 400 or 500 range.
         return false;
